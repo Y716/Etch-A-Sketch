d7993493d8a4e909cda1d334f040c523f5a5c24a
Yasin Hakim <ibnkhaqim@gmail.com>
2025-05-10 23:52:40 +0700

    Etch-A-Sketch V0.1

--- script.js
+++ script.js
@@ -3,11 +3,37 @@ rect = container.getBoundingClientRect()
 console.log("Width: " + container.clientWidth + "px");
 console.log("Height: " + container.clientHeight+ "px");
 // container.textContent = "Container"
-for (let index = 0; index < 16*16; index++) {
-    const div = document.createElement("div");
-    div.style.width = container.clientWidth/16 + "px";
-    div.style.height = container.clientHeight/16 + "px";
-    // console.log("Width: " + div.style.width + "px");
-    console.log("Height: " + div.style.height+ "px");
-    container.appendChild(div)
+gridSize = 16;
+generateGrid();
+
+function generateGrid(){
+    while(container.hasChildNodes()){
+        container.removeChild(container.firstChild);
+    }
+    for (let index = 0; index < gridSize*gridSize; index++) {
+        const div = document.createElement("div");
+        div.style.width = container.clientWidth/gridSize + "px";
+        div.style.height = container.clientHeight/gridSize + "px";
+        div.onmouseover = function(){
+            div.style.backgroundColor = "white";
+        }
+        // console.log("Width: " + div.style.width + "px");
+        console.log("Height: " + div.style.height+ "px");
+        container.appendChild(div)
+    }
 }
+
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    newGridSize = prompt("Enter your size", gridSize);
+    if(newGridSize > 100){
+        alert("The Size is too big!")
+    }else{
+        gridSize = newGridSize;
+        generateGrid();
+    }
+    
+})
+
